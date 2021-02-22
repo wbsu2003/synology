15f17614f04b85155ca3381da3f7897288f081f1
老苏 <wbsu2003@users.noreply.github.com>
2021-02-22 23:37:49 +0800

    Update synology-alertover.php

--- synology-alertover.php
+++ synology-alertover.php
@@ -11,19 +11,19 @@ $localOnly = true;
 echo time();
 // Validate httpHost and/or remote addr?
 if ($localOnly) {
-    if ($_SERVER['HTTP_HOST'] != 'localhost') {
-        // Not locahost
-        echo 'Not locahost';
-        die;
-    }
+  if ($_SERVER['HTTP_HOST'] != 'localhost') {
+    // Not locahost
+    echo 'Not locahost';
+    die;
+  }
 }
 
 // Set variables
 $options = array(
-    'source' => isset($_GET['source']) ? $_GET['source'] : false,
-    'receiver' => isset($_GET['receiver']) ? $_GET['receiverid'] : false,
-    'content' => isset($_GET['content']) ? $_GET['content'] : false,
-    'title' => isset($_GET['title']) ? $_GET['title'] : false
+  'source' => isset($_GET['source']) ? $_GET['source'] : false,
+  'receiver' => isset($_GET['receiver']) ? $_GET['receiver'] : false,
+  'content' => isset($_GET['content']) ? $_GET['content'] : false,
+  'title' => isset($_GET['title']) ? $_GET['title'] : false
 );
 // Remove empty values
 $options = array_filter($options);
@@ -31,15 +31,15 @@ $options = array_filter($options);
 
 // Quit if not exactly 4 get values were found
 if (count($options) != 4) {
-    echo 'invalid options';
-    die;
+  echo 'invalid options';
+  die;
 }
 
 // Do Alertovover curl
 curl_setopt_array($ch = curl_init(), array(
-    CURLOPT_URL => "https://api.alertover.com/v1/alert",
-    CURLOPT_POSTFIELDS => $options,
-    CURLOPT_SAFE_UPLOAD => true,
+  CURLOPT_URL => "https://api.alertover.com/v1/alert",
+  CURLOPT_POSTFIELDS => $options,
+  CURLOPT_SAFE_UPLOAD => true,
 ));
 curl_exec($ch);
 curl_close($ch);
