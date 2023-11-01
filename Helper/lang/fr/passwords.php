ce8297ca3c7042150b75b9c87b0b7a223f33419a
老苏 <wbsu2003@users.noreply.github.com>
2023-11-01 22:23:56 +0800

    Create passwords.php

---
+++ Helper/lang/fr/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '您的密码已被重置！',
+    'sent' => '我们已通过电子邮件向您发送了重置密码的链接！',
+    'throttled' => '请稍后再试。',
+    'token' => '此密码重置令牌无效。',
+    'user' => '找不到使用此电子邮件地址的用户。',
+];
