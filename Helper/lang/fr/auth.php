2914653e007b6580d0306e0e405e1c23ac5b5241
老苏 <wbsu2003@users.noreply.github.com>
2023-11-01 22:22:18 +0800

    Create auth.php

---
+++ Helper/lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '这些凭据与我们的记录不匹配。',
+    'password' => '提供的密码不正确。',
+    'throttle' => '登录尝试次数过多，请在 :seconds 秒后重试。',
+
+];
